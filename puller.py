a073aa3a62baac19bef2000c4a8a856a8bc0d894
YukkuriC <799433638@qq.com>
2022-05-31 15:41:10 +0800

    puller: add auto-clear

--- puller.py
+++ puller.py
@@ -18,6 +18,14 @@ except:
     exit()
 
 base_dir = os.path.abspath(os.path.join(__file__, '..'))
+# remove old
+for root, folders, files in os.walk(base_dir):
+    for f in files:
+        if f.endswith('.js'):
+            os.remove(os.path.join(root, f))
+    if not os.listdir(root):
+        os.removedirs(root)
+# create new
 for obj in raw:
     obj = obj['data']
     path = os.path.abspath(os.path.join(base_dir, obj['filename'].lstrip('/')))
