fe2b49b0b3df3f0663928fa408e86374f8d96ae8
YukkuriC <799433638@qq.com>
2026-07-12 19:29:26 +0800

    totally forgot there's .git, damn

--- TOOLS/common.py
+++ TOOLS/common.py
@@ -2,7 +2,16 @@ import os
 
 base_dir = os.path.abspath(os.path.join(__file__, '../..'))
 
-ignored_dirs = [os.path.join(base_dir, f) for f in ['TOOLS', 'old']]
+ignored_dirs = [
+    os.path.join(base_dir, f)
+    for f in [
+        'TOOLS',
+        'old',
+        # oh damn
+        'node_modules',
+        '.git',
+    ]
+]
 
 
 def is_script(filename):
@@ -11,8 +20,22 @@ def is_script(filename):
     return filename.endswith('.js') or filename.endswith('.ts')
 
 
-def walk_without_ignored():
-    for root, folders, files in os.walk(base_dir):
-        if any(os.path.samefile(root, i) for i in ignored_dirs):
-            continue
-        yield root, folders, files
+def walk_without_ignored(root=base_dir):
+    folders, files = [], []
+    folders_full = []
+    for p in os.listdir(root):
+        pp = os.path.join(root, p)
+        if os.path.isdir(pp) and not any(os.path.samefile(pp, i) for i in ignored_dirs):
+            folders.append(p)
+            folders_full.append(pp)
+        else:
+            files.append(p)
+    yield root, folders, files
+    for sub in folders_full:
+        yield from walk_without_ignored(sub)
+
+
+if __name__ == '__main__':
+    for root, folders, files in walk_without_ignored():
+        jss = [f for f in files if is_script(f)]
+        print(root, jss)
