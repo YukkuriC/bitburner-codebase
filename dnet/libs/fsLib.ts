4bfb5f4942420177572377797c6ea4fd2d0bd8b3
YukkuriC <799433638@qq.com>
2026-07-13 15:03:12 +0800

    why recording those changing random stuff?

--- dnet/libs/fsLib.ts
+++ dnet/libs/fsLib.ts
@@ -1,36 +1,25 @@
 // special lib for persistent data IO
 export const STORE_TARGET = 'darkweb'
 
-export function pathProbe(host: string) {
-    return `/dnet_probes/${host}.txt`
-}
-export function pathPassword(host: string) {
-    return `/dnet_password/${host}.txt`
-}
-
-function readFileFromStore(ns: NS, path: string) {
-    const success = ns.scp(path, ns.getHostname(), STORE_TARGET)
-    if (!success) return null
-    return ns.read(path).trim()
-}
-function writeFileToStore(ns: NS, path: string, content: string) {
-    ns.write(path, content, 'w')
-    ns.scp(path, STORE_TARGET)
-}
+const mapProbe = globalThis.mapProbe ?? {}
+const mapPassword = globalThis.mapPassword ?? {}
 
 export function getProbes(ns: NS, host: string) {
-    const raw = readFileFromStore(ns, pathProbe(host))
+    const raw = mapProbe[host]
     if (typeof raw !== 'string') return []
     return raw.split('\n').filter((x) => x.trim())
 }
 export function getPassword(ns: NS, host: string) {
-    const raw = readFileFromStore(ns, pathPassword(host))
+    const raw = mapPassword[host]
     if (typeof raw !== 'string') return null
     return raw
 }
 export function setProbes(ns: NS, host: string, probes: string[]) {
-    writeFileToStore(ns, pathProbe(host), probes.join('\n'))
+    mapProbe[host] = probes.join('\n')
 }
 export function setPassword(ns: NS, host: string, pw: string) {
-    writeFileToStore(ns, pathPassword(host), pw)
+    mapPassword[host] = pw
 }
+
+globalThis.mapProbe = mapProbe
+globalThis.mapPassword = mapPassword
