e81d05ef32a701fdd030d4e0200f7b12a802a278
YukkuriC <799433638@qq.com>
2026-07-14 22:55:14 +0800

    simple locking

--- dnet/daemon.ts
+++ dnet/daemon.ts
@@ -6,6 +6,7 @@ import { call, callWait } from './libs/callLib'
 export async function main(ns: NS) {
     const { dnet } = ns
     const host = ns.getHostname()
+    let first = true
     while (1) {
         const probes = (await ns.dnet.probe()).filter((x) => {
             if (x === host) return false
@@ -15,14 +16,18 @@ export async function main(ns: NS) {
         for (const sub of probes) {
             if (sub === 'darkweb') continue
             if (ns.scriptRunning(ns.getScriptName(), sub)) continue
-            await callWait(ns, 'hack', host, [sub])
-            const pw = fs.getPassword(sub)
-            if (typeof pw !== 'string') continue
-            await dnet.connectToSession(sub, pw)
-            await callWait(ns, 'copier', host, [sub, pw])
-            await callWait(ns, 'postHack', sub)
+            if (fs.lock(host, first)) {
+                await callWait(ns, 'hack', host, [sub])
+                const pw = fs.getPassword(sub)
+                if (typeof pw !== 'string') continue
+                await dnet.connectToSession(sub, pw)
+                await callWait(ns, 'copier', host, [sub, pw])
+                await callWait(ns, 'postHack', sub)
+                fs.unlock(host)
+            }
         }
 
         await dnet.nextMutation()
+        first = false
     }
 }
--- dnet/libs/fsLib.ts
+++ dnet/libs/fsLib.ts
@@ -1,9 +1,20 @@
 // special lib for persistent data IO
-export const STORE_TARGET = 'darkweb'
 
 const mapProbe = globalThis.mapProbe ?? {}
 const mapPassword = globalThis.mapPassword ?? {}
+const mapLocks = globalThis.mapLocks ?? {}
 
+// single-thread lock
+export function lock(host: string, steal = false) {
+    if (mapLocks[host] && !steal) return false
+    mapLocks[host] = true
+    return true
+}
+export function unlock(host: string, steal = false) {
+    mapLocks[host] = false
+}
+
+// data
 export function getProbes(host: string) {
     const raw = mapProbe[host]
     if (typeof raw !== 'string') return []
@@ -23,3 +34,4 @@ export function setPassword(host: string, pw: string) {
 
 globalThis.mapProbe = mapProbe
 globalThis.mapPassword = mapPassword
+globalThis.mapLocks = mapLocks
