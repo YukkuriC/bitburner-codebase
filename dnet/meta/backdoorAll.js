8e913bdf34a4e64194a972f2a126b1fafa3a8f37
YukkuriC <799433638@qq.com>
2026-07-14 22:00:31 +0800

    exploit backdoor script

---
+++ dnet/meta/backdoorAll.js
@@ -0,0 +1,30 @@
+import { bfsCall } from '../../BASE'
+const SuperTerminal = terminal.meta
+
+/** @param {NS} ns */
+export async function main(ns) {
+    const servers = {}
+    const getServer = (host) => {
+        if (!servers[host]) {
+            servers[host] = SuperTerminal.getServer(host)
+        }
+        return servers[host]
+    }
+
+    let cnt = 0
+    await bfsCall(
+        (host) => {
+            const server = getServer(host)
+            const subs = server.serversOnNetwork
+            return subs.filter((s) => ns.dnet.isDarknetServer(s))
+        },
+        (host) => {
+            const server = getServer(host)
+            if (!server.backdoorInstalled) cnt++
+            server.backdoorInstalled = true
+            ns.print('backdooring: ' + host)
+            server.isStationary = host === 'darkweb' || !!ns.args[0]
+        },
+    )
+    ns.tprint(`Total ${cnt} backdoored`)
+}
